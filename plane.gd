4d4dacd46c35a962d68897f1b31edb29ff11d072
Earvizu17 <edgar.arvizu@bsu.edu>
2024-09-18 23:22:11 -0400

    plane mod

--- plane.gd
+++ plane.gd
@@ -4,13 +4,12 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("flap"): 
-		self.apply_force(Vector2(0, -15250))
+		self.linear_velocity = Vector2(self.linear_velocity.x, -100)  # Set a consistent upward velocity.
 		flappyblueplane.play("flap")
 
 
